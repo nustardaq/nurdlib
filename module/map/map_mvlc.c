ede48891afc144cb357a84c951d603a799066631
Anna Kawecka <anna.kawecka@chalmers.se>
2024-10-01 09:40:12 +0200

    Added POKE to map_mvlc

--- module/map/map_mvlc.c
+++ module/map/map_mvlc.c
@@ -30,11 +30,13 @@
 #	include <mvlcc_wrap.h>
 #	include <nurdlib/log.h>
 #	include <nurdlib/config.h>
-#	include <nurdlib/log.h>
 #	include <util/string.h>
 #	include <util/time.h>
 
 static void	mvlcc_init(void);
+
+#define Write 1
+#define Read 2
 
 int ec;
 mvlcc_t mvlc;
@@ -78,10 +80,6 @@ uint32_t
 sicy_r32(struct Map *a_map, size_t a_ofs)
 {
 	uint32_t u32;
-
-	(void) a_map;
-	(void) a_ofs;
-	
 	u32 = 0;
 	
 	ec = mvlcc_single_vme_read(mvlc, a_map->address + a_ofs, &u32, 32, 32);
@@ -93,10 +91,6 @@ uint16_t
 sicy_r16(struct Map *a_map, size_t a_ofs)
 {
 	uint32_t u32;
-
-	(void) a_map;
-	(void) a_ofs;
-
 	u32 = 0;
 
 	ec = mvlcc_single_vme_read(mvlc, a_map->address + a_ofs, &u32, 32, 16);
@@ -108,17 +102,15 @@ sicy_r16(struct Map *a_map, size_t a_ofs)
 void
 sicy_w32(struct Map *a_map, size_t a_ofs, uint32_t a_u32)
 {
-	(void) a_map;
-	(void) a_ofs;
-	(void) a_u32;
+	ec = mvlcc_single_vme_write(mvlc, a_map->address + a_ofs, a_u32, 32, 32);
 }
 
 void
 sicy_w16(struct Map *a_map, size_t a_ofs, uint16_t a_u16)
 {
-	(void) a_map;
-	(void) a_ofs;
-	(void) a_u16;
+	uint32_t a_u32;
+	a_u32 = (uint32_t)a_u16;
+	ec = mvlcc_single_vme_write(mvlc, a_map->address + a_ofs, a_u32, 32, 16);
 }
 
 MAP_FUNC_EMPTY(sicy_setup);
@@ -129,11 +121,37 @@ UNMAP_FUNC_EMPTY(sicy);
 
 #define POKE(Mode, m, value) \
 { \
- (void)value; \
- (void)a_address; \
- (void)a_bits; \
- (void)a_ofs; \
+	LOGF(verbose)(LOGL, "poke_" #m " %u bits {", a_bits); \
+	mvlcc_init(); \
+	switch (a_bits) { \
+	case 16: { \
+		uint32_t u32 = (uint32_t)value;	\
+		if (Mode == Read) { \
+			ec = mvlcc_single_vme_read(mvlc, a_address + a_ofs, &u32, 32, 16); \
+		} else if (Mode == Write) { \
+			ec = mvlcc_single_vme_write(mvlc, a_address + a_ofs, u32, 32, 16); \
+		} else { \
+			log_die(LOGL, "Invalid mode."); \
+		} \
+		} \
+		break; \
+	case 32: { \
+		uint32_t u32_ = value; \
+		if (Mode == Read) { \
+			ec = mvlcc_single_vme_read(mvlc, a_address + a_ofs, &u32_, 32, 32); \
+		} else if (Mode == Write) { \
+			ec = mvlcc_single_vme_write(mvlc, a_address + a_ofs, u32_, 32, 32); \
+		} else { \
+			log_die(LOGL, "Invalid mode."); \
+		} \
+		} \
+		break; \
+	default: \
+		log_die(LOGL, "Poking %u bits unsupported.", a_bits); \
+	} \
+	LOGF(verbose)(LOGL, "poke_" #m " }"); \
 }
+
 
 void
 poke_r(uint32_t a_address, uintptr_t a_ofs, unsigned a_bits)
