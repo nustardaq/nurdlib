09e5b157201c039bddb0be79af48830d3e5f25da
Hans Toshihide Törnqvist <hans.tornqvist@chalmers.se>
2024-11-01 15:09:00 +0100

    Missing copyrights.

--- mock/tridi/trloctrl/fw_cacad0d0_tridi/tridi_defs.h
+++ mock/tridi/trloctrl/fw_cacad0d0_tridi/tridi_defs.h
@@ -1,3 +1,25 @@
+/*
+ * nurdlib, NUstar ReaDout LIBrary
+ *
+ * Copyright (C) 2024
+ * Hans Toshihide Törnqvist
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301  USA
+ */
+
 typedef struct tridi_scaler_map_t
 {
   /*   79 0x613c */ uint32_t mux_src[101];
