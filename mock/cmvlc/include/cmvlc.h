e24bdc9505402d3a029c84418679e50352fddad5
DAQ <daq-nonexist-email@curie.fy.chalmers.se>
2025-05-17 19:40:37 +0200

    Mock: cmvlc_block_get().

--- mock/cmvlc/include/cmvlc.h
+++ mock/cmvlc/include/cmvlc.h
@@ -57,5 +57,7 @@ int			cmvlc_single_vme_write(struct cmvlc_client *,
     uint32_t, uint32_t, enum cmvlc_vme_addr_width, enum cmvlc_vme_data_width);
 int			cmvlc_set_daq_mode(struct cmvlc_client *, int, int,
     uint8_t (*)[2], int, uint8_t);
+int			cmvlc_block_get(struct cmvlc_client *,
+    const uint32_t *, size_t,  size_t *, uint32_t *, size_t, size_t *);
 
 #endif
