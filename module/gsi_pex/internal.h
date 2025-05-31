16d6ecf46c2c15bc7f0107ebde7894fa70c33983
Hans Toshihide Törnqvist <h.toernqvist@gsi.de>
2025-05-31 20:46:49 +0200

    New GSI Pex v5 registers.

--- module/gsi_pex/internal.h
+++ module/gsi_pex/internal.h
@@ -59,8 +59,13 @@ struct GsiPexDma {
 	uint32_t	dst;
 	uint32_t	transport_size;
 	uint32_t	burst_size;
-	uint32_t	stat;
+	uint32_t	stat;     /* aka dma_ctl */
+	/* new pex5 registers */
+	uint32_t	irq_ctl;  /* unused */
+	uint32_t	irq_stat; /* unused */
+	uint32_t	dst_high;
 };
+
 struct GsiPexGoc {
 	enum	GsiPexGocCmd cmd;
 	uint8_t	sfp_i;
