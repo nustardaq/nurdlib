444404f917ebc45bd7b906678c246e4d9edfb371
DAQ <daq-nonexist-email@curie.fy.chalmers.se>
2025-06-21 01:37:44 +0200

    Mock: drasi/lwroc_parse_util.h

---
+++ mock/drasi/lwroc_parse_util.h
@@ -0,0 +1,31 @@
+/*
+ * nurdlib, NUstar ReaDout LIBrary
+ *
+ * Copyright (C) 2024
+ * Hans Toshihide Törnqvist
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301  USA
+ */
+
+#ifndef LWROC_PARSE_UTIL_H
+#define LWROC_PARSE_UTIL_H
+
+#define LWROC_MATCH_C_PREFIX(prefix,post)               \
+  (strncmp(request,prefix,strlen(prefix)) == 0 &&       \
+   *(post = request + strlen(prefix)) != '\0')
+#define LWROC_MATCH_C_ARG(name) (strcmp(request,name) == 0)
+
+#endif
